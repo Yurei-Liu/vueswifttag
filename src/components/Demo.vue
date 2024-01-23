a56d900812f0e16c531fd0fd0f490672fb0e643e
Areis_Liu <71692471+Yurei-Liu@users.noreply.github.com>
2024-01-23 16:03:09 +0800

    新增 VueSwiftTag.vue 和 Demo.vue

---
+++ src/components/Demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Demo",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/VueSwiftTag.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "VueSwiftTag",
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
