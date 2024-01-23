86bd761dd6e62bd100fbed6a9fdd9eed2ae41a0f
Areis_Liu <71692471+Yurei-Liu@users.noreply.github.com>
2024-01-23 18:02:04 +0800

    完成 VueSwiftTag 與 Demo 頁面

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,12 +1,61 @@
 <template>
-  <div></div>
+  <div style="width: 80%">
+    <VueSwiftTag :swiftTagItems="swiftTagItems" />
+
+    <div id="auditArea" style="height: 500px; margin-bottom: 3%; margin-top: 3%; border: 1px solid; border-radius: 5px">
+      <p style="margin-left: 1%">發查審核區</p>
+      <hr />
+      <div></div>
+    </div>
+    <div id="basicInfo" style="height: 500px; margin-bottom: 3%; border: 1px solid; border-radius: 5px">
+      <p style="margin-left: 1%">案件基本資訊</p>
+      <hr />
+      <div></div>
+    </div>
+    <div id="customerData" style="height: 500px; margin-bottom: 3%; border: 1px solid; border-radius: 5px">
+      <p style="margin-left: 1%">客戶資料區</p>
+      <hr />
+      <div></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import VueSwiftTag from "./VueSwiftTag.vue";
+
 export default {
   name: "Demo",
   data() {
-    return {};
+    return {
+      /**
+       * swiftTagObj:{
+       *   order: 1,
+       *   id: areaId,
+       *   name: "快捷測試1"
+       * }
+       */
+      swiftTagItems: [
+        {
+          order: 1,
+          id: "auditArea",
+          name: "發查審核區",
+        },
+        {
+          order: 2,
+          id: "basicInfo",
+          name: "案件基本資訊",
+        },
+        {
+          order: 3,
+          id: "customerData",
+          name: "客戶資料區",
+        },
+      ],
+    };
+  },
+
+  components: {
+    VueSwiftTag,
   },
 };
 </script>
--- src/components/VueSwiftTag.vue
+++ src/components/VueSwiftTag.vue
@@ -1,16 +1,106 @@
 <template>
-  <div></div>
+  <div class="mt-1" style="width: 80%; display: inline-flex">
+    <template v-if="swiftTagItems.length > 0">
+      <div
+        v-for="(tagItem, index) in swiftTagItems"
+        :key="index"
+        class="fixed-bookmark"
+        :style="'order:' + tagItem.order"
+        @click="toBlock(tagItem.id)"
+      >
+        <i class="fa-light fa-paper-plane mr-1" /> {{ tagItem.name }}
+      </div>
+    </template>
+    <template v-else>
+      <p>目前尚無快捷標籤</p>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
   name: "VueSwiftTag",
+  props: ["swiftTagItems", "tagBackgroundColor", "tagBorderColor", "tagFontColor", "tagHoverBackgroundColor", "tagHoverFontColor"],
   data() {
-    return {};
+    return {
+      /**
+       * swiftTagObj:{
+       *   order: 1,
+       *   id: areaId,
+       *   name: "快捷測試1"
+       * }
+       */
+    };
   },
 
-  methods: {},
+  mounted() {
+    this.styleInit();
+  },
+
+  methods: {
+    styleInit() {
+      // 主要底色
+      if (this.tagBackgroundColor) {
+        document.getElementsByTagName("body")[0].style.setProperty("--mainBackgroundColor", this.tagBackgroundColor);
+      }
+      // 主要border顏色
+      if (this.tagBorderColor) {
+        document.getElementsByTagName("body")[0].style.setProperty("--mainBorderColor", this.tagBorderColor);
+      }
+
+      // 主要font color
+      if (this.tagFontColor) {
+        document.getElementsByTagName("body")[0].style.setProperty("--mainColor", this.tagFontColor);
+      }
+
+      // hover底色
+      if (this.tagHoverBackgroundColor) {
+        document.getElementsByTagName("body")[0].style.setProperty("--hoverBackgroundColor", this.tagHoverBackgroundColor);
+      }
+
+      // hover color
+      if (this.tagHoverFontColor) {
+        document.getElementsByTagName("body")[0].style.setProperty("--hoverColor", this.tagHoverFontColor);
+      }
+    },
+
+    toBlock(id) {
+      document.querySelector("#" + id).scrollIntoView({ behavior: "smooth" });
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// 主要底色
+$mainBackgroundColor: var(--mainBackgroundColor, #416364);
+// 主要border顏色
+$mainBorderColor: var(--mainBorderColor, #586853);
+// 主要font color
+$mainColor: var(--mainColor, white);
+
+// hover底色
+$hoverBackgroundColor: var(--hoverBackgroundColor, #f8e4ae);
+// hover color
+$hoverColor: var(--hoverColor, #29140a);
+
+.fixed-bookmark {
+  background-color: $mainBackgroundColor;
+  border: 0.5px solid $mainBorderColor;
+  color: $mainColor;
+  border-radius: 10px;
+  padding: 3px 10px;
+  font-size: 13px;
+  margin: 3px;
+
+  transition: 0.3s;
+  box-shadow: 1px 2px 2px rgb(177, 177, 177);
+}
+
+.fixed-bookmark:hover {
+  background-color: $hoverBackgroundColor;
+  color: $hoverColor;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
